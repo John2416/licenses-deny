ff85bf1e7a72e4ab5c7cdbaae25a44d3a92b1d5b
John <lung-sleeve-yoyo@duck.com>
2026-01-17 15:49:36 +0800

    feat! merge from old repo

---
+++ src/licenses_deny/__init__.py
@@ -0,0 +1,6 @@
+"""Package initialization for licenses_deny."""
+
+from .cli import main
+
+__all__ = ["main"]
+__version__ = "0.1.0"
---
+++ src/licenses_deny/__main__.py
@@ -0,0 +1,7 @@
+"""Execute licenses_deny as a module."""
+
+from .cli import main
+
+
+if __name__ == "__main__":
+    main()
